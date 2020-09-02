01984ca8e8463c59270234ad5b5441339fdb6f11
Valary Lim Wan Qian <valarylim@outlook.com>
2020-09-02 15:45:04 +0800

    valary | combine conventional news to single df

--- scraping/main_conventional.py
+++ scraping/main_conventional.py
@@ -1,9 +1,9 @@
 import pandas as pd
 from datetime import datetime
+
+from google import google_scrape
 from theguardian import theguardian_scrape
 from cryptocontrol import cryptocontrol_scrape
-# from google import google_scrape
-
 
 def conventional_scrape_by_entity(entity, start_date, end_date):
     column_names = ["date_time", "title", "excerpt", "domain", \
@@ -11,29 +11,49 @@ def conventional_scrape_by_entity(entity, start_date, end_date):
     combined_df = pd.DataFrame(columns = column_names)
 
     # retrieve data
+    # google: ['date_time', 'title', 'excerpt', 'domain', 'article_url']
     # theguardian: ["date_time", "title", "excerpt", "article_url"]
     # cryptocontrol: ["date_time", "title", "excerpt", "domain", "article_url", "image_url", "hotness", "activity_hotness"]
+    google_df = google_scrape(entity, start_date, end_date)
     theguardian_df = theguardian_scrape(entity, start_date, end_date)
-    cryptocontrol_df = cryptocontrol_scrape(entity)
+    theguardian_df["domain"] = "theguardian"
 
-    # filter cryptocontrol_df based on data that is within start and end date
-    mask = (cryptocontrol_df['date_time'] >= start_date) & (cryptocontrol_df['date_time'] <= end_date)
-    cryptocontrol_df = cryptocontrol_df[mask]
+    cryptocontrol_scraped = False
+    try:
+        cryptocontrol_df = cryptocontrol_scrape(entity)
+        # filter cryptocontrol_df based on data within start and end date
+        mask = (cryptocontrol_df['date_time'] >= start_date) & (cryptocontrol_df['date_time'] <= end_date)
+        cryptocontrol_df = cryptocontrol_df[mask] 
+        cryptocontrol_scraped = True
+    except:
+        pass
 
     # combine the dataframes
-    combined_df = cryptocontrol_df.copy(deep=True)
-    print(len(combined_df), len(theguardian_df))
-    combined_df = combined_df.merge(theguardian_df, how="outer")
-    print(len(combined_df))
-    print(combined_df)
+    if cryptocontrol_scraped:
+        combined_df = pd.concat([cryptocontrol_df, google_df, theguardian_df])
+    else: 
+        combined_df = pd.concat([google_df, theguardian_df])
     
-    return 
+    return combined_df
 
 def conventional_scrape(entity_list, start_date, end_date):
-    return 
+    result_df = pd.DataFrame()
+    for i in range(len(entity_list)):
+        temp_df = conventional_scrape_by_entity(entity=entity_list[i], start_date=start_date, end_date=end_date)
+        temp_df["entity"] = entity_list[i]
+        if i == 0:
+            result_df = temp_df
+        else:
+            result_df = result_df.merge(temp_df, how="outer")
+    
+    # drop columns where all rows are nan
+    result_df = result_df.dropna(axis=1, how='all')
 
-entity = "ethereum"
-start_date = datetime(2019, 9, 1, 0, 0, 0)
-end_date = datetime(2020, 9, 2, 23, 59, 59)
+    return result_df
 
-conventional_scrape_by_entity(entity=entity, start_date=start_date, end_date=end_date)
+# entity = "ethereum"
+# start_date = datetime(2019, 8, 1, 0, 0, 0)
+# end_date = datetime(2020, 9, 2, 23, 59, 59)
+# entity_list = ["binance", "ethereum", "upbit"]
+# print(conventional_scrape_by_entity(entity="ethereum", start_date=start_date, end_date=end_date))
+# print(conventional_scrape(entity_list=entity_list, start_date=start_date, end_date=end_date))
