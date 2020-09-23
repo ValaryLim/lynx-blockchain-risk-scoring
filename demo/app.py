5cc623a8d9bc1e00fd235ef9342f07da908ee032
Valary Lim Wan Qian <valarylim@outlook.com>
2020-09-24 00:31:57 +0800

    valary | add code to read crypto data

--- demo/app.py
+++ demo/app.py
@@ -1,3 +1,4 @@
+# import packages
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
@@ -7,6 +8,16 @@ from dash.dependencies import Input, Output, State
 import pandas as pd
 from datetime import datetime
 
+# data
+import sys
+sys.path.insert(1, '../scraping')
+from main_conventional import conventional_scrape_by_entity
+from main_crypto import crypto_scrape_by_entity
+
+# models
+import fasttext
+
+# set application
 app = dash.Dash(__name__, external_stylesheets=["assets/datepicker.css", dbc.themes.BOOTSTRAP])
 
 #### STYLES ###################################################################
@@ -27,6 +38,7 @@ CONTENT_STYLE = {
     "margin-left": "30rem",
     "margin-right": "2rem",
     "padding": "2rem 1rem",
+    "font-size": "12px"
 }
 
 #### SIDEBAR ##################################################################
@@ -42,12 +54,12 @@ model_input = html.Div([
     dbc.Label("Model", className="p", style={"font-weight": "600"}),
     dbc.RadioItems(
         options=[
-            {"label": "BERT", "value": 1},
-            {"label": "FastText", "value": 2},
-            {"label": "Word-2-Vec", "value": 3},
-            {"label": "Vader", "value": 4},
+            {"label": "BERT", "value": "bert"},
+            {"label": "FastText", "value": "fasttext"},
+            {"label": "Word-2-Vec", "value": "wordvec"},
+            {"label": "Vader", "value": "vader"},
         ],
-        id="model-input", value=1, inline=True,
+        id="model-input", value="bert", inline=True,
         style={"font-size": "14px"})
 ], style={"font-size": "14px"})
 
@@ -80,47 +92,60 @@ sidebar = html.Div(
 )
 
 #### CONTENT ##################################################################
-def generate_table(dataframe, max_rows=10):
-    return html.Table([
-        html.Thead(
-            html.Tr([html.Th(col) for col in dataframe.columns])
-        ),
-        html.Tbody([
-            html.Tr([
-                html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
-            ]) for i in range(min(len(dataframe), max_rows))
-        ])
-])
-
-# table for conventional news
-
-content = html.Div([html.P("hello")], id="page-content", style=CONTENT_STYLE)
+content = html.Div([
+    html.Div(id="conventional-news"),
+    html.Div(id="reddit-news"),
+    html.Div(id="twitter-news"),
+], id="page-content", style=CONTENT_STYLE)
 
 #### LAYOUT ###################################################################
 
-app.layout = html.Div([dcc.Location(id="url"), sidebar, content])
+app.layout = html.Div([dcc.Location(id="url"), sidebar, \
+    dbc.Spinner(content, spinner_style={"width": "3rem", "height": "3rem"})])
 
 #### CALLBACKS ################################################################
+def generate_table(name, dataframe, max_rows=10):
+    '''
+    generate table dynamically from dataframe
+    '''
+    return html.Div([html.H5(name),
+        dbc.Table([
+            html.Thead(
+                html.Tr([html.Th(col) for col in dataframe.columns])
+            ),
+            html.Tbody([
+                html.Tr([
+                    html.Td(dataframe.iloc[i][col]) for col in dataframe.columns
+                ]) for i in range(min(len(dataframe), max_rows))
+            ])
+        ])
+    ])
+
 # when submit button is pressed, run query
 @app.callback(
-    Output("page-content", "children"), 
+    [Output("conventional-news", "children"),
+    Output("reddit-news", "children"),
+    Output("twitter-news", "children")], 
     [Input("submit", "n_clicks")],
     [State("entity-input", "value"), 
     State("model-input", "value"), 
     State("date-input", "start_date"),
     State("date-input", "end_date")]
 )
-def render_page_content(entity, model, start_date, end_date, n_clicks):
+def render_page_content(n_clicks, entity, model, start_date, end_date):
     if n_clicks == None:
-        return None
-    return html.Div([
-        html.P(entity),
-        html.P(str(model)),
-        html.P(start_date),
-        html.P(str(type(start_date))),
-        html.P(end_date),
-        html.P(n_clicks)
-    ])
+        return (None, None, None)
+
+    # convert start and end date to datetime
+    start_date_datetime = datetime.strptime(start_date, "%Y-%m-%d")
+    end_date_datetime = datetime.strptime(end_date, "%Y-%m-%d")
+    end_date_datetime = end_date_datetime.replace(hour=23, minute=59)
+    
+    # retrieve data based on information
+    crypto_df = conventional_scrape_by_entity(entity, start_date_datetime, end_date_datetime)
+    crypto_df = pd.concat([crypto_df, crypto_scrape_by_entity(entity, start_date_datetime, end_date_datetime)])[["date_time", "title"]]
+
+    return (generate_table("Conventional and Cryptonews", crypto_df), None, None)
 
 if __name__ == "__main__":
     app.run_server(debug=True, host='127.0.0.1')
