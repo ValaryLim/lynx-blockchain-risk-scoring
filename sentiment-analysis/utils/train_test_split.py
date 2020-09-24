d6784c7def42b04e0660fdd321c29ae3be4d5277
leejx9 <60956726+leejx9@users.noreply.github.com>
2020-09-24 17:19:17 +0800

    jingxuan | updated train_test split by date

--- sentiment-analysis/utils/train_test_split.py
+++ sentiment-analysis/utils/train_test_split.py
@@ -14,78 +14,35 @@ def try_parsing_date(text):
     raise ValueError('no valid date format found')
 
 
-#Return train set (60%), validation set(20%), test set(20%)
-def train_val_test_split(filepath):
-    df = pd.read_csv(filepath, low_memory = False, encoding= 'latin-1')
 
-    #Convert all date_time to datetime format
-    df['date_time'] = df['date_time'].apply(lambda x: try_parsing_date(x))
-    df = df[df['date_time'].notna()]
-
-    
-    #Sort df by date_time
-    df = df.sort_values('date_time', ascending = True)
-    df = df.reset_index(drop=True)
-
-    #Num of rows in the dataframe
-    numrows = df.shape[0]
-
-    #Retrieve just the date 
-    df['date'] =  df['date_time'].apply(lambda x: x.date())
-
-    #Get training dataset
-    #Get last date for the train set and get the data rows that fall within the date range
-    train_index = int(0.6*numrows) - 1
-    last_date1 = df.iloc[train_index]['date']
-    train = df[df['date'] <= last_date1]
-    train = train.drop(['date','Unnamed: 0'], axis = 1)
-    
-    #Get validation dataset
-    num_rows_required = int(0.2*numrows) 
-    val_index = train.shape[0]
-    last_date2 = df.iloc[val_index + num_rows_required - 1]['date']
-    mask = (df['date'] > last_date1) & (df['date'] <= last_date2)
-    val = df.loc[mask]
-    val = val.drop(['date','Unnamed: 0'], axis = 1)
-
-    #Get testing dataset
-    test = df[df['date'] > last_date2]
-    test = test.drop(['date','Unnamed: 0'], axis = 1)
-
-    return train, val, test
-
-
-
-
-#Return train set (80%), test set(20%)
 def train_test_split(filepath):
     df = pd.read_csv(filepath, low_memory = False, encoding= 'latin-1')
 
+    cols = list(df.columns) 
+    print(cols)
+
+    if 'text' not in cols:
+        df['excerpt'] = df['excerpt'].fillna('')
+        df['text'] = df.title + '. ' + df.excerpt
+
+
     #Convert all date_time to datetime format
     df['date_time'] = df['date_time'].apply(lambda x: try_parsing_date(x))
     df = df[df['date_time'].notna()]
 
     #Sort df by date_time
     df = df.sort_values('date_time', ascending = True)
-    df = df.reset_index(drop=True)
 
-    #Num of rows in the dataframe
-    numrows = df.shape[0]
-
-    #Retrieve just the date 
-    df['date'] =  df['date_time'].apply(lambda x: x.date())
+    last_date = datetime(2019,5,14,23,59,59)
 
     #Get training dataset
     #Get last date for the train set and get the rows that fall within the date range
-    train_index = int(0.8*numrows) - 1
-    last_date = df.iloc[train_index]['date']
-    train = df[df['date'] <= last_date]
-    train = train.drop(['date','Unnamed: 0'], axis = 1)
+    train = df[df['date_time'] <= last_date]
+    train = train[['date_time','label','text']]
 
     #Get testing dataset
-    test = df[df['date'] > last_date]
-    test = test.drop(['date','Unnamed: 0'], axis = 1)
+    test = df[df['date_time'] > last_date]
+    test = test[['date_time','label','text']]
 
     return train, test
-
 
