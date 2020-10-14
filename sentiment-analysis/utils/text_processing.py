0e2f43ccb36777c30db799639190de28b53faaad
Valary Lim Wan Qian <valarylim@outlook.com>
2020-10-15 02:48:11 +0800

    valary | add more text processing features

--- sentiment-analysis/utils/text_processing.py
+++ sentiment-analysis/utils/text_processing.py
@@ -1,3 +1,4 @@
+import re
 import pandas as pd
 import string
 import nltk
@@ -5,10 +6,24 @@ from nltk.stem import WordNetLemmatizer # word lemmatizer
 from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import stopwords # stopwords
 
-def text_processing(text, lower=True, remove_punctuation=True, \
-    remove_stopwords=True, remove_entity=True, lemmatize=True, stem=False):
+def is_alphanumeric(text):
+    return any(char.isdigit() for char in text) and any(char.isalpha() for char in text)
+
+def is_transaction_hash(text):
+    return is_alphanumeric(text) and len(text) > 20
+
+def text_processing(text, 
+                    lower=True, 
+                    remove_url=True, 
+                    remove_punctuation=True, 
+                    remove_stopwords=False, 
+                    replace_entity=False, 
+                    replace_hash=False,
+                    split_alphanumeric=False,
+                    lemmatize=False,
+                    stem=False):
     '''
-    Accepts a text and processes text
+    Accepts a text and options to run the following processing functions:
     '''
     # strip non-ascii characters
     text = text.encode('ascii', errors='ignore')
@@ -18,14 +33,18 @@ def text_processing(text, lower=True, remove_punctuation=True, \
     if lower:
         text = text.lower()
 
-    # remove unnecessary new lines and whitespaces
-    text = text.replace("\n", "") 
-    text = ' '.join(text.split())
-
+    # remove url 
+    if remove_url:
+        text = re.sub(r'http\S+', '', text)
+    
     # remove punctuation
     if remove_punctuation:
         text = text.translate(str.maketrans(string.punctuation, ' '*len(string.punctuation)))
-
+        
+    # remove unnecessary new lines and whitespaces
+    text = text.replace("\n", "") 
+    text = ' '.join(text.split())
+    
     # tokenize
     text_words = nltk.word_tokenize(text)
 
@@ -48,10 +67,25 @@ def text_processing(text, lower=True, remove_punctuation=True, \
             stop = set(stop)
         text_words = [x for x in text_words if not x.lower() in stop]
     
-    # remove entity name from text
+    # replace entity
     entity_list = set(pd.read_csv("../data/entity_list.csv", header=0)["entity"])
     entity_list = set(x.lower() for x in entity_list) # convert to lowercase
-    if remove_entity:
-        text_words = [x for x in text_words if not x.lower() in entity_list]
-
+    if replace_entity:
+        text_words = [x if not (x.lower() in entity_list) else "entity" for x in text_words]
+        
+    # replace transaction hashes
+    if replace_hash:
+        text_words = [x if not is_transaction_hash(x) else "hash" for x in text_words]
+    
+    # split alphanumeric numbers
+    updated_words = []
+    if split_alphanumeric:
+        for word in text_words:
+            if (word != "2fa") and is_alphanumeric(word) and (word[0].isdigit() or word[-1].isdigit()): # first or last is digit
+                new_word = re.findall('\d+|\D+', word)
+                updated_words.extend(new_word)
+            else:
+                updated_words.append(word)
+        text_words = updated_words
+    
     return ' '.join(text_words)
