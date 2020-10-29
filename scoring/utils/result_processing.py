e647d4863256b0669537653e33a80e29fc659b84
Valary Lim Wan Qian <valarylim@outlook.com>
2020-10-30 03:25:35 +0800

    valary | scoring exploration and implementation

---
+++ scoring/utils/result_processing.py
@@ -0,0 +1,82 @@
+import pandas as pd
+import datetime as datetime
+
+def process_dataframe(df):
+    if "date" in df.columns:
+        df["date_time"] = df["date"]
+    
+    # change entity to lowercase
+    df["entity"] = df["entity"].apply(lambda x: x.lower())
+
+    # format date_time
+    df = format_date_time(df)
+
+    # retrieve date
+    df["date"] = df.date_time.apply(lambda x: x.date())
+    
+    # select specific columns
+    df = df[["date_time", "date", "text", "entity", "prob", "pred"]]
+
+    # process duplicates
+    df = process_duplicates(df, subset=["text", "entity"])
+    
+    return df
+
+def format_date_time(df):
+    '''
+    Formats date_time column into datetime object
+    '''
+    for fmt in ('%Y-%m-%d %H:%M:%S', '%d/%m/%y %H:%M', '%d/%m/%y'):
+        try:
+            df['date_time'] = pd.to_datetime(df.date_time, format=fmt)
+        except ValueError:
+            pass
+    
+    return df
+
+
+def process_duplicates(df, subset=["text", "entity"]):
+    '''
+    Accepts a pandas dataframe and outputs 
+    '''
+    if df.empty:
+        df["count"] = 0
+        df["date_time_all"] = []
+        return df
+    
+    # assign count based on text and entity
+    df["counter"] = df.groupby(by=subset).text.transform('size')
+
+    # get array of all date times
+    df = df.merge(df.groupby(subset).date_time.agg(list).reset_index(), 
+              on=subset, 
+              how='left',
+                  suffixes=['', '_all'])
+    
+    # drop duplicates, keeping the first
+    df = df.drop_duplicates(subset=subset, keep='first')
+
+    return df
+
+def process_duplicates_entity_independent(df):
+    '''
+    Accepts a pandas dataframe and outputs 
+    '''
+    if df.empty:
+        df["counter"] = 0
+        df["date_first"] = ""
+        return df
+    
+    # assign count based on text and entity
+    df["counter"] = df.groupby(by=["text"]).text.transform('size')
+
+    # get array of all date times
+    df = df.merge(df.groupby(["text"]).date_time.agg(list).reset_index(), 
+              on=["text"], 
+              how="left",
+                  suffixes=['', '_all'])
+    
+    # drop duplicates, keeping the first
+    df = df.drop_duplicates(subset=["text"], keep='first')
+
+    return df
