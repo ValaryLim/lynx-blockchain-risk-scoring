cb1e9a4ba923ba5a45d448ed1c4de037329d28d5
laiyanjean <laiyanjean@gmail.com>
2020-11-04 02:25:03 +0800

    yanjean | added overall demonstration tab to dashboard

--- demo/app.py
+++ demo/app.py
@@ -11,7 +11,9 @@ import pandas as pd
 from datetime import datetime
 
 # set application
-app = dash.Dash(__name__, external_stylesheets=["assets/datepicker.css", dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, suppress_callback_exceptions=True, \
+                external_stylesheets=["assets/datepicker.css", dbc.themes.BOOTSTRAP])
+
 
 #### STYLES ###################################################################
 # style arguments for the sidebar
@@ -39,29 +41,68 @@ SCORE_STYLE = {
     "font-size": "15px", 
     "font-weight": "600", 
     "background-color": "#f1f4f9", 
+    "border-color": "white",
     "height": "80px", 
-    "border-radius": "25px", 
+    # "border-radius": "10px", 
     "text-align": "center"
-
 }
+
+# styles for the side tabs
+TAB_STYLE = {
+    "font-size": "20px", 
+    "font-weight": "600", 
+    "text-align": "left"
+}
+
+unclicked_button_style = {'background-color': '#f1f4f9',
+                            'border': '#f1f4f9',
+                            'color': 'black',
+                            'height': '50px',
+                            'width': '100%',
+                            'margin-top': '0px',
+                            'margin-left': '0px'}
+
+clicked_button_style = {'background-color': 'white',
+                        'border': '#f1f4f9',
+                        'color': 'darkblue',
+                        'height': '50px',
+                        'width': '100%',
+                        'margin-top': '0px',
+                        'margin-left': '0px'}
+
 
 #### SIDEBAR ##################################################################
 sidebar = html.Div(
     [
-        html.P("Lynx Demo", className="display-4"),
+        html.P("CyLynx Demo", className="display-4"),
         html.H6("Demonstration Dashboard for BT4103", className="lead"),
         html.Hr(),
+        html.Br(),
+        # dcc.Link(html.H6('Overall Demonstration', style=TAB_STYLE), href='overall'),
+        dcc.Link(
+            html.Button(html.H6("Overall Demonstration", style=TAB_STYLE), id='overall-button',
+                n_clicks=0,
+                style=unclicked_button_style), href='overall'
+        ),
+        html.Br(),
+        dcc.Link(
+            html.Button(html.H6("Entity Demonstration", style=TAB_STYLE), id='entity-button',
+            n_clicks=0,
+            style=unclicked_button_style), href='entity'
+        ),
     ],
     style=SIDEBAR_STYLE,
 )
 
-#### CONTENT ##################################################################
+
+#### ENTITY PAGE CONTENT ##################################################################
 # retrieve entity list
 entity_list = list(pd.read_csv('data/entity_list.csv').entity)
 entity_list.sort() # sort in alphabetical order
+
 # entity input
 entity_input = html.Div([
-    dbc.Label("Entity Name", className="p", style={"font-weight": "600"}),
+    dbc.Label("Entity Name", className="p", style={"font-weight": "600", "font-size": "14px"}),
     html.Br(),
     dcc.Dropdown(
         id='entity-input',
@@ -77,7 +118,7 @@ entity_input = html.Div([
 
 # date input
 date_input = html.Div([
-    dbc.Label("Date Range", className="p", style={"font-weight": "600"}),
+    dbc.Label("Date Range", className="p", style={"font-weight": "600", "font-size": "14px"}),
     html.Br(),
     dcc.DatePickerRange(
         id="date-input",
@@ -96,9 +137,9 @@ submit_button = html.Div([
     dbc.Button("Submit", color="dark", block=False, id="submit", className="mb-3")
 ], style={'width': '20', 'display': 'inline-block'})
 
-content = html.Div(
+# entity page layout
+entity_content = html.Div(
     [
-
         dbc.Row([entity_input, date_input, submit_button]),
         html.Hr(),
         html.Br(),
@@ -112,21 +153,26 @@ content = html.Div(
         html.Div(id="twitter-news"),
         
     ], 
-    id="page-content", style=CONTENT_STYLE,
+    id="entity-page-content", style=CONTENT_STYLE,
 )
 
-#### LAYOUT ###################################################################
 
-app.layout = html.Div([dcc.Location(id="url"), sidebar, \
-    dbc.Spinner(content, spinner_style={"width": "3rem", "height": "3rem"})])
+#### APP LAYOUT ###################################################################
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    sidebar,
+    dbc.Spinner(id='page-content', spinner_style={"width": "3rem", "height": "3rem"})
+])
 
-#### CALLBACKS ################################################################
+
+#### ENTITY PAGE CALLBACKS ################################################################
 
 def generate_table(name, dataframe):
     '''
     generate table dynamically from dataframe
     '''
-    return html.Div([html.Br(),
+    return html.Div([
+        html.Br(),
         html.H5(name),
         dbc.Label(f"Total Articles Retrieved: {len(dataframe)}", 
                     style={"font-weight": "600", "font-size": "14px"}),
@@ -171,9 +217,6 @@ def generate_graph(entity, start_date, end_date):
     # transform data
     graph_data = pd.DataFrame(columns=['date', 'score', 'source'])
 
-    # mean score over time
-    mean = sample_risk_data[['score']].mean(axis=1)
-
     for col in ['news_score', 'reddit_score', 'twitter_score', 'score']:
         source_data = sample_risk_data[['date', col]]
         if col == 'score':
@@ -208,7 +251,7 @@ def generate_graph(entity, start_date, end_date):
     Output("count-graph", "children"),
     Output("conventional-news", "children"),
     Output("reddit-news", "children"),
-    Output("twitter-news", "children")], 
+    Output("twitter-news", "children")],
     [Input("submit", "n_clicks")],
     [State("entity-input", "value"), 
     State("date-input", "start_date"),
@@ -216,7 +259,7 @@ def generate_graph(entity, start_date, end_date):
     ]
 )
 
-def render_page_content(n_clicks, entity, start_date, end_date):
+def render_entity_page(n_clicks, entity, start_date, end_date):
     if n_clicks == None:
         return (None, None, None, None, None)
 
@@ -226,16 +269,52 @@ def render_page_content(n_clicks, entity, start_date, end_date):
     end_date_datetime = end_date_datetime.replace(hour=23, minute=59)
 
     ##### SCORE DISPLAY #####
+    # read csv of risk data
+    sample_risk_data = pd.read_csv("data/entity_risk_score_2020.csv")
+    # extract relevant date and entity
+    sample_risk_data = sample_risk_data.loc[sample_risk_data['entity']==entity.lower()]
+    sample_risk_data = sample_risk_data[(sample_risk_data.date >= start_date) & (sample_risk_data.date <= end_date)]
+    # max score over time
+    max_score = pd.DataFrame(sample_risk_data.max(axis=0)).iloc[2:6]
+    max_score.columns = ['max_score']
+    max_score['max_score'] = [round(float(x), 2) for x in max_score['max_score']]
+
+    def get_color(score):
+        if score >= 75:
+            return 'red'
+        elif score >= 50:
+            return 'orange'
+        else:
+            return 'black'
+
     score_display = html.Div([
         dbc.Label("Open Source Information", className="p", style={"font-size": "30px", "font-weight": "600", 'display': 'inline-block', 'margin-right': 50}),
-        dbc.Label(("Overall Score: 100"), className="p", style={"font-size": "15px", "font-weight": "600", 'width': '20%', 'display': 'inline-block', "background-color": "#D77560", "height": "30px", "border-radius": "25px", "text-align": "center"}),
+        dbc.Label((f"Overall Score: {str(max_score.loc['score', 'max_score'])}"), 
+                    className="p", style={"font-size": "15px", "font-weight": "600", 'width': '20%', 'display': 'inline-block', "background-color": "#D77560", "height": "30px", "border-radius": "25px", "text-align": "center"}),
         html.Br(),
         html.Br(),
         dbc.Row(
             [
-                dbc.Col(html.Div("News", style=SCORE_STYLE)),
-                dbc.Col(html.Div("Reddit", style=SCORE_STYLE)),
-                dbc.Col(html.Div("Twitter", style=SCORE_STYLE)),
+                dbc.Col([
+                    html.Div("News"),
+                    html.Br(),
+                    html.Div(str(max_score.loc['news_score', 'max_score']), 
+                    style={'font-size': '18px', 'color': get_color(max_score.loc['news_score', 'max_score'])})],
+                    style=SCORE_STYLE),
+
+                dbc.Col([
+                    html.Div("Reddit"),
+                    html.Br(),
+                    html.Div(str(max_score.loc['reddit_score', 'max_score']), 
+                    style={'font-size': '18px', 'color': get_color(max_score.loc['reddit_score', 'max_score'])})],
+                    style=SCORE_STYLE),
+
+                dbc.Col([
+                    html.Div("Twitter"),
+                    html.Br(),
+                    html.Div(str(max_score.loc['twitter_score', 'max_score']), 
+                    style={'font-size': '18px', 'color': get_color(max_score.loc['twitter_score', 'max_score'])})],
+                    style=SCORE_STYLE),
             ]
         ),   
     ]
@@ -279,6 +358,140 @@ def render_page_content(n_clicks, entity, start_date, end_date):
     twitter_table = generate_table("Twitter", twitter_df.rename(columns=columns))
 
     return (score_display, count_graph, crypto_table, reddit_table, twitter_table)
+
+
+#### OVERALL PAGE CONTENT ##################################################################
+
+overall_content = html.Div(
+    [
+        dbc.Row([date_input, submit_button]),
+        html.Hr(),
+        # html.Br(),
+        # html.Br(),
+        # html.Div(id = "overall-score-display"),
+        # html.Br(),
+        html.Br(),
+        html.Div(id = "overall-graph"),    
+        html.Div(id = "overall-table")    
+    ], 
+    id="overall-page-content", style=CONTENT_STYLE,
+)
+
+#### OVERALL PAGE CALLBACKS ################################################################
+def generate_overall_table(name, dataframe):
+    '''
+    generate table dynamically from dataframe
+    '''
+    return html.Div([
+        html.Br(),
+        html.H5(name),
+        html.Br(),
+        html.Div(
+            dash_table.DataTable(
+                columns=[
+                    {"name": i, "id": i, "selectable":False} for i in dataframe.columns
+                ],
+                data=dataframe.to_dict('records'),
+                page_action='none',
+                fixed_rows={'headers': True},
+                style_table={'height': '300px', 'overflowY': 'auto'},
+                style_data={'whiteSpace': 'normal', 'height': 'auto'},
+                style_cell={
+                    'height': 'auto',
+                    # all three widths are needed
+                    # 'minWidth': '80px', 'width': '150px', 'maxWidth': '180px',
+                    'whiteSpace': 'normal',
+                    'textAlign': 'left',
+                    'fontSize': 12, 
+                    'font-family':'Verdana',
+                },
+                css=[{'selector': '.row', 'rule': 'margin: 0'}],
+            ), 
+            style={})
+])
+
+@app.callback(
+    [# Output("overall-score-display", "children"),
+    Output("overall-graph", "children"),
+    Output("overall-table", "children")],
+    [Input("submit", "n_clicks")],
+    [State("date-input", "start_date"),
+    State("date-input", "end_date")
+    ]
+)
+
+def render_overall_page(n_clicks, start_date, end_date):
+    if n_clicks == None:
+        # return (None, None, None)
+        return (None, None)
+
+    ##### SCORE DISPLAY #####
+    # read csv of risk data
+    sample_risk_data = pd.read_csv("data/entity_risk_score_2020.csv")
+    # extract relevant date and entity
+    sample_risk_data = sample_risk_data[(sample_risk_data.date >= start_date) & (sample_risk_data.date <= end_date)]
+    
+    '''
+    sample_risk_data_slice = sample_risk_data.loc[sample_risk_data['entity']=='overall']
+    # max score over time
+    max_score = pd.DataFrame(sample_risk_data_slice.max(axis=0)).iloc[2:6]
+    max_score.columns = ['max_score']
+    max_score['max_score'] = [round(float(x), 2) for x in max_score['max_score']]
+
+    overall_score_display = html.Div([
+        dbc.Label("Open Source Information", className="p", style={"font-size": "30px", "font-weight": "600", 'display': 'inline-block', 'margin-right': 50}),
+        dbc.Label((f"Overall Score: {str(max_score.loc['score', 'max_score'])}"), 
+                    className="p", style={"font-size": "15px", "font-weight": "600", 'width': '20%', 'display': 'inline-block', "background-color": "#D77560", "height": "30px", "border-radius": "25px", "text-align": "center"}),
+        html.Br(),
+        html.Br(),
+        dbc.Row(
+            [
+                dbc.Col(html.Div(f"News: {str(max_score.loc['news_score', 'max_score'])}"), style=SCORE_STYLE),
+                dbc.Col(html.Div(f"Reddit: {str(max_score.loc['reddit_score', 'max_score'])}", style=SCORE_STYLE)),
+                dbc.Col(html.Div(f"Twitter: {str(max_score.loc['twitter_score', 'max_score'])}", style=SCORE_STYLE)),
+            ]
+        ),   
+    ]
+    )
+    '''
+
+    ##### GRAPH #####
+    overall_graph = html.Div(generate_graph('overall', str(start_date),str(end_date)))
+
+    ##### TABLE #####
+    # create overall data
+    overall_data = sample_risk_data.loc[sample_risk_data['entity'] != 'overall']
+    # extract relevant columns to display
+    overall_data = overall_data[['date', 'entity', 'score']]
+    overall_data = overall_data.round({'score': 2})
+    # sort by score
+    overall_data = overall_data.sort_values(by=['score'], ascending=False)
+    # rename columns
+    overall_data = overall_data.rename(columns={"date": "Date", "score": "Score", "entity": "Entity"})
+
+    overall_table = generate_overall_table("High Risk Entities by Overall Score", overall_data)
+
+    # return (overall_score_display, overall_graph, overall_table)
+    return (overall_graph, overall_table)
+
+
+#### RENDER PAGE CONTENT ################################################################
+
+# Update the index
+@app.callback(
+    [
+    Output('overall-button', 'style'),
+    Output('entity-button', 'style'),
+    Output('page-content', 'children')],
+    Input('url', 'pathname')
+)
+
+def render_page_content(pathname):
+    if pathname == '/entity':
+        return (unclicked_button_style, clicked_button_style, entity_content)
+
+    else: # the default is always the overall
+        return (clicked_button_style, unclicked_button_style, overall_content)
 
 if __name__ == "__main__":
     app.run_server(debug=True, host='127.0.0.1')
